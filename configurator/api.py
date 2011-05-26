e0d25220a4e6019b9cf62e68f7e99b4a413d40fb
Chris Withers <chris@simplistix.co.uk>
2011-05-26 07:45:01 +0100

    another guess at an internal API

---
+++ configurator/api.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2011 Simplistix Ltd
+# See license.txt for license details.
+
+class Node(object):
+
+    def set(self,name,value,source=None):
+        pass
+
+    def append(self,name,value,source=None):
+        pass
+
+    def remove(self,name,value,source=None):
+        pass
+
