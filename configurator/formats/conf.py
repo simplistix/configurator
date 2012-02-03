974ca61530103c3fa2ea683c7703bb15e8b8e1fb
Chris Withers <chris@simplistix.co.uk>
2012-02-03 07:58:36 +0000

    start of a sketch for the .conf parser.

---
+++ configurator/formats/conf.py
@@ -0,0 +1,12 @@
+from configurator.api import API
+
+def parse(source,text):
+    node = API()
+    for i,line in enumerate(text.split('\n')):
+        line_no = i+1
+        line = line.strip()
+        if not line:
+            continue
+        key,value = line.split(' ',1)
+        node.set(key,value.strip(),'line %i of %s' % (line_no,source))
+    return node
