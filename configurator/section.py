05d3e5b89c4b12a968562b5831bccd8c01360eaf
Chris Withers <chris@simplistix.co.uk>
2012-06-28 07:36:15 +0100

    Correct spacing.

--- configurator/section.py
+++ configurator/section.py
@@ -1,30 +1,30 @@
-# Copyright (c) 2011 Simplistix Ltd
+# Copyright (c) 2011-2012 Simplistix Ltd
 # See license.txt for license details.
 
-from . import _marker
+from . import marker
 from .api import API
 
 class Section(object):
 
-    def __init__(self):
-        self._api = API()
+    def __init__(self, source=None):
+        self._api = API(source)
 
-    def __getitem__(self,name):
+    def __getitem__(self, name):
         return self._api.get(name)
     
-    def __setitem__(self,name,value):
+    def __setitem__(self, name, value):
         self._api.set(name,value)
 
-    def get(self,name,default=None):
-        return self._api.get(name,default)
+    def get(self, name, default=None):
+        return self._api.get(name, default)
     
-    def __getattr__(self,name):
-        value = self._api.get(name,_marker)
-        if value is _marker:
+    def __getattr__(self, name):
+        value = self._api.get(name, marker)
+        if value is marker:
             raise AttributeError(name)
         return value
 
-    def __setattr__(self,name,value):
-        if name=='_api':
-            self.__dict__[name]=value
-        self._api.set(name,value)
+    def __setattr__(self, name, value):
+        if name == '_api':
+            self.__dict__[name] = value
+        self._api.set(name, value)
