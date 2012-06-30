e5a42a4cab18efdc7b67c54bfbe7e516f610b702
Chris Withers <chris@simplistix.co.uk>
2012-06-30 12:05:40 +0100

    Section API implemented, needs API docs.

--- configurator/_section.py
+++ configurator/_section.py
@@ -3,17 +3,20 @@
 
 from . import marker
 from ._api import API
+from ._utils import get_source
 
 class Section(object):
-
     def __init__(self, source=None):
-        self._api = API(source)
+        self._api = API(source or get_source())
 
     def __getitem__(self, name):
-        return self._api.get(name)
+        value =  self._api.get(name, marker)
+        if value is marker:
+            raise KeyError(name)
+        return value
     
     def __setitem__(self, name, value):
-        self._api.set(name,value)
+        self._api.set(name, value, source=get_source())
 
     def get(self, name, default=None):
         return self._api.get(name, default)
@@ -27,4 +30,21 @@ class Section(object):
     def __setattr__(self, name, value):
         if name == '_api':
             self.__dict__[name] = value
-        self._api.set(name, value)
+        else:
+            self._api.set(name, value, get_source())
+
+    def keys(self):
+        for a in self._api.items():
+            if a.name:
+                yield a.name
+
+    def values(self):
+        for a in self._api.items():
+            yield a.value
+
+    def items(self):
+        for a in self._api.items():
+            yield a.name, a.value
+
+    def __iter__(self):
+        return self.keys()
--- tests/test_section.py
+++ tests/test_section.py
@@ -2,47 +2,120 @@
 # See license.txt for license details.
 
 from unittest import TestCase
-from testfixtures import compare,ShouldRaise,Comparison as C
+from testfixtures import compare, ShouldRaise, Comparison as C
 
-class Tests(TestCase):
+from configurator import api
+from configurator._section import Section
+
+from .common import SourceMixin
+
+class Base(SourceMixin):
 
     def setUp(self):
-        from configurator import api
-        from configurator._section import Section
+        super(Base, self).setUp()
         self.s = Section()
         self.a = api(self.s)
 
+class Tests(Base, TestCase):
+
     # simple access
     
+    def test_dict_get(self):
+        self.a.set('foo', 'v')
+        compare(self.s.get('foo'), 'v')
+
+    def test_dict_get_none_value(self):
+        self.a.set('foo', None)
+        compare(self.s.get('foo'), None)
+
     def test_dict_get_not_there(self):
-        compare(self.s.get('foo'),None)
+        compare(self.s.get('foo'), None)
 
     def test_dict_get_not_there_default(self):
-        compare(self.s.get('foo','bar'),'bar')
+        compare(self.s.get('foo','bar'), 'bar')
 
     def test_dict_getitem(self):
-        self.s['foo']='bar'
-        compare(self.s['foo'],'bar')
+        self.a.set('foo', 'bar')
+        compare(self.s['foo'], 'bar')
+
+    def test_dict_getitem_none_value(self):
+        self.a.set('foo', None)
+        compare(self.s['foo'], None)
+
+    def test_dict_getitem_not_there(self):
+        with ShouldRaise(KeyError('foo')):
+            self.s['foo']
 
     def test_getattr(self):
-        self.s['foo']='bar'
-        compare(self.s.foo,'bar')
+        self.a.set('foo', 'bar')
+        compare(self.s.foo, 'bar')
 
     def test_getattr_nothere(self):
         with ShouldRaise(AttributeError('foo')):
             self.s.foo
 
     def test_getattr_default(self):
-        compare(getattr(self.s,'foo','bar'),'bar')
+        compare(getattr(self.s, 'foo', 'bar'), 'bar')
 
-    # setting
+    # simple setting
     
     def test_setattr(self):
         self.s.foo='bar'
-        compare(self.s.foo,'bar')
-        
+        compare(self.a.get('foo'), 'bar')
+
     def test_tree(self):
-        from configurator._section import Section
         child = Section()
         self.s.child = child
         self.assertTrue(self.s.child is child)
+
+    def test_set_special_name(self):
+        self.s.get = 'foo'
+        compare(self.a.get('get'), 'foo')
+        compare(self.s.get('get'), 'foo')
+        compare(self.s['get'], 'foo')
+        
+    # source
+    def test_setattr_source(self):
+        self.s.foo='bar'
+        compare(self.a.source('foo'), 'default_source')
+
+    def test_setitem_source(self):
+        self.s['foo']='bar'
+        compare(self.a.source('foo'), 'default_source')
+
+    def test_section_source(self):
+        s = Section('my source')
+        compare(api(s).source(), 'my source')
+
+    def test_section_source_default(self):
+        s = Section()
+        compare(api(s).source(), 'default_source')
+
+class TestIteration(Base):
+    
+    # iteration
+
+    def setUp(self):
+        super(TestIteration, self).setUp()
+        self.a.set('k1', 'v1')
+        self.a.append('v2')
+        self.a.set('k3', 'v3')
+    
+    def test_iteration(self):
+        compare(['k1', 'k3'], list(self.s))
+
+    def test_iteration_items(self):
+        actual = []
+        for name, value in self.s.items():
+            actual.append((name, value))
+        compare([
+            ('k1', 'v1'),
+            (None, 'v2'),
+            ('k3', 'v3'),
+            ], actual)
+        
+    def test_iteration_keys(self):
+        compare(['k1', 'k3'], list(self.s.keys()))
+
+    def test_iteration_values(self):
+        compare(['v1', 'v2', 'v3'], list(self.s.values()))
