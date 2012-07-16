8a7ecb0be2f1a7af05e0763b856aea8165c57f44
Chris Withers <chris@simplistix.co.uk>
2012-07-16 07:22:33 +0100

    Allow get_source to work on multiple levels.

--- configurator/tests/test_utils.py
+++ configurator/tests/test_utils.py
@@ -19,3 +19,19 @@ class TestUtils(TestCase):
             level1(),
             "File %r, line 13, in level1" % file
             )
+
+    def test_get_source_level_supplied(self):
+        def level3():
+            return get_source(3)
+        def level2():
+            return level3()
+        def level1():
+            return level2()
+        if __file__.endswith('.pyc'):
+            file = __file__[:-1] # pragma: no cover
+        else:
+            file = __file__ # pragma: no cover
+        self.assertEqual(
+            level1(),
+            "File %r, line 29, in level1" % file
+            )
