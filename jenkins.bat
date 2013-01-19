281a4ba2a0aafbe2fd383d84472e5c894099ab11
Chris Withers <chris@simplistix.co.uk>
2013-01-19 21:56:00 +0000

    no point building an sdist if the tests fail

--- jenkins.bat
+++ jenkins.bat
@@ -1,5 +1,5 @@
 %PYTHON_EXE% bootstrap.py
 CHOICE /T 20 /C w /D w
 bin\buildout
-bin\docpy setup.py sdist
 bin\nosetests --with-xunit --with-cov --cov=configurator --cov-report=xml --cov-report=html
+bin\docpy setup.py sdist
