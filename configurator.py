28d8a5784a5588f43134cf4667c28200e7a8c055
Chris Withers <chris@simplistix.co.uk>
2017-07-25 07:54:06 +0100

    Config node that supports attribute and dict behaviour.

--- configurator.py
+++ configurator.py
@@ -3,7 +3,66 @@ from os.path import expanduser
 import yaml
 
 
-class Config(object):
+class ConfigNode(object):
+
+    def __init__(self, data=None):
+        if data is None:
+            data = {}
+        self.data = data
+
+    def _get(self, name):
+        try:
+            value = self.data[name]
+        except TypeError:
+            raise KeyError(name)
+        if isinstance(value, (dict, list)):
+            value = ConfigNode(value)
+        return value
+
+    def __getattr__(self, name):
+        try:
+            return self._get(name)
+        except KeyError:
+            raise AttributeError(name)
+
+    def __getitem__(self, name):
+        return self._get(name)
+
+    def get(self, name, default=None):
+        try:
+            return self._get(name)
+        except KeyError:
+            return default
+
+    def items(self):
+        for key, value in sorted(self.data.items()):
+            if isinstance(value, (dict, list)):
+                value = ConfigNode(value)
+            yield key, value
+
+    def __iter__(self):
+        for item in self.data:
+            yield item
+    #
+    # def merge(self, other):
+    #     if not isinstance(other, Config):
+    #         raise TypeError('{!r} is not a Config instance'.format(other))
+    #     data = other.data
+    #     if type(data) is not type(self.data):
+    #         raise TypeError('Cannot merge {} with {}'.format(
+    #             type(data), type(self.data)
+    #         ))
+    #     elif isinstance(data, list):
+    #         self.data.extend(data)
+    #     elif isinstance(data, dict):
+    #         for key, value in other.data.items():
+    #             self.data[key] = self._merge(self.data[key])
+    #             self.data.update(data)
+    #     else:
+    #         raise TypeError('Cannot merge {!r}'.format(data))
+
+
+class Config(ConfigNode):
 
     def __init__(self, data=None):
         if data is None:
@@ -23,27 +82,4 @@ class Config(object):
         with open(expanduser(path)) as stream:
             return cls.from_stream(stream)
 
-    def __getattr__(self, name):
-        return self.data[name]
 
-    def __getitem__(self, name):
-        return self.data[name]
-
-    def __iter__(self):
-        for item in self.data:
-            yield item
-
-    def validate(self, schema):
-        schema(self.data)
-
-    def merge(self, other):
-        if not isinstance(other, Config):
-            raise TypeError('{!r} is not a Config instance'.format(other))
-        data = other.data
-        if isinstance(self.data, list):
-            self.data.extend(data)
-        else:
-            self.data.update(data)
-
-    def insert(self, key, other):
-        raise NotImplementedError()
--- tests/test_node.py
+++ tests/test_node.py
@@ -1,69 +1,152 @@
-from io import StringIO
-from tempfile import NamedTemporaryFile
-from unittest import TestCase
-
 from testfixtures import compare, ShouldRaise
 
-from configurator import Config
+from configurator import ConfigNode
 
 
-class InstantiationTests(TestCase):
+class TestInstantiation(object):
 
     def test_empty(self):
-        config = Config()
-        # make sure empty dict is default:
-        config.merge(Config({'x': 1}))
-        compare(config.x, 1)
-
-    def test_path(self):
-        with NamedTemporaryFile() as source:
-            source.write('{"x": 1}')
-            source.flush()
-            config = Config.from_file(source.name)
-        compare(config.x, expected=1)
-
-    def test_stream(self):
-        with StringIO(u'{"x": 1}') as source:
-            config = Config.from_stream(source)
-        compare(config.x, expected=1)
+        config = ConfigNode()
+        compare(config.data, expected={})
 
     def test_dict(self):
-        config = Config(dict(x=1))
-        compare(config.x, expected=1)
+        config = ConfigNode(dict(x=1))
+        compare(config['x'], expected=1)
 
     def test_list(self):
-        config = Config([1, 2])
+        config = ConfigNode([1, 2])
         compare(config[0], expected=1)
         compare(config[1], expected=2)
         compare(list(config), expected=[1, 2])
 
     def test_int(self):
-        # not very useful...
-        config = Config(1)
+        # not very useful, but documents .data as a public api
+        config = ConfigNode(1)
+        compare(config.data, 1)
 
 
-class MergeTests(TestCase):
+class TestDictAccess(object):
 
-    def test_empty(self):
-        config = Config()
-        config.merge(Config())
-        # make sure empty dict is default:
-        config.merge(Config({'x': 1}))
-        compare(config.x, 1)
+    def test_there_dict(self):
+        config = ConfigNode({'foo': 1})
+        compare(config['foo'], expected=1)
 
-    def test_non_config(self):
-        config = Config()
-        with ShouldRaise(TypeError("'foo' is not a Config instance")):
-            config.merge('foo')
-
-    def test_dict_to_dict(self):
-        config = Config()
-        config.merge(Config(dict(x=1)))
-        compare(config.x, expected=1)
-
-    def test_list_to_list(self):
-        config = Config([1, 2])
-        config.merge(Config([3, 4]))
+    def test_there_list(self):
+        config = ConfigNode([1])
         compare(config[0], expected=1)
-        compare(config[2], expected=3)
-        compare(list(config), expected=[1, 2, 3, 4])
+
+    def test_not_there_dict(self):
+        config = ConfigNode({})
+        with ShouldRaise(KeyError('foo')):
+            config['foo']
+
+    def test_not_there_list(self):
+        config = ConfigNode([])
+        with ShouldRaise(IndexError('list index out of range')):
+            config[0]
+
+    def test_not_there_simple(self):
+        config = ConfigNode(1)
+        with ShouldRaise(KeyError('foo')):
+            config['foo']
+
+    def test_get(self):
+        config = ConfigNode({'foo': 1})
+        compare(config.get('foo'), expected=1)
+
+    def test_get_default(self):
+        config = ConfigNode({})
+        compare(config.get('foo', 1), expected=1)
+
+    def test_get_default_default(self):
+        config = ConfigNode({})
+        compare(config.get('foo'), expected=None)
+
+    def test_get_list_number_key(self):
+        config = ConfigNode([1])
+        compare(config.get(0), expected=1)
+
+    def test_get_list_string_key(self):
+        config = ConfigNode([1])
+        compare(config.get('foo'), expected=None)
+
+    def test_simple_value(self):
+        config = ConfigNode({})
+        compare(config.get('foo', 1), expected=1)
+
+    def test_dict_value(self):
+        config = ConfigNode({'foo': {'bar': 1}})
+        obj = config['foo']
+        assert isinstance(obj, ConfigNode)
+        compare(obj.data, expected={'bar': 1})
+
+    def test_list_value(self):
+        config = ConfigNode({'foo': [1]})
+        obj = config['foo']
+        assert isinstance(obj, ConfigNode)
+        compare(obj.data, expected=[1])
+
+    def test_get_wrapped_value(self):
+        config = ConfigNode({'foo': {'bar': 1}})
+        obj = config['foo']
+        assert isinstance(obj, ConfigNode)
+        compare(obj.data, expected={'bar': 1})
+
+    def test_items_dict(self):
+        config = ConfigNode({'foo': 1, 'bar': 2})
+        compare(config.items(), expected=[('bar', 2), ('foo', 1)])
+
+    def test_items_list(self):
+        config = ConfigNode([])
+        expected = "'list' object has no attribute 'items'"
+        with ShouldRaise(AttributeError(expected)):
+            tuple(config.items())
+
+    def test_items_wrapped_value(self):
+        config = ConfigNode({'foo': [1]})
+        obj = tuple(config.items())[0][1]
+        assert isinstance(obj, ConfigNode)
+        compare(obj.data, expected=[1])
+
+
+class TestAttributeAccess(object):
+
+    def test_there(self):
+        config = ConfigNode({'foo': 1})
+        compare(config.foo, expected=1)
+
+    def test_not_there(self):
+        config = ConfigNode({})
+        with ShouldRaise(AttributeError('foo')):
+            config.foo
+
+    def test_simple_value(self):
+        config = ConfigNode({'foo': 1})
+        compare(config.foo, expected=1)
+
+    def test_dict_value(self):
+        config = ConfigNode({'foo': {'bar': 1}})
+        obj = config.foo
+        assert isinstance(obj, ConfigNode)
+        compare(obj.data, expected={'bar': 1})
+
+    def test_list_value(self):
+        config = ConfigNode({'foo': [1]})
+        obj = config.foo
+        assert isinstance(obj, ConfigNode)
+        compare(obj.data, expected=[1])
+
+    def test_name_clash_with_real_attrs(self):
+        # method, data, __method
+        config = ConfigNode({'get': 1})
+        compare(config.get('get'), expected=1)
+
+    def test_list_source(self):
+        config = ConfigNode([1])
+        with ShouldRaise(AttributeError('foo')):
+            config.foo
+
+    def test_simple_source(self):
+        config = ConfigNode(1)
+        with ShouldRaise(AttributeError('foo')):
+            config.foo
