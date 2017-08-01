6641e6b25968592579babd796b969c538bb1f2cd
Chris Withers <chris@simplistix.co.uk>
2017-08-01 08:11:51 +0100

    Implement the store side of mapping.

--- configurator/mapping.py
+++ configurator/mapping.py
@@ -1,8 +1,113 @@
-from functools import partial
-from operator import itemgetter, attrgetter
+class ItemOp(object):
+
+    def __init__(self, text):
+        self.text = text
+
+    def get(self, data):
+        return data[self.text]
+
+    def ensure(self, data):
+        try:
+            return data[self.text]
+        except KeyError:
+            data[self.text] = value ={}
+            return value
+
+    def set(self, data, value):
+        data[self.text] = value
 
 
-class LoadPath(object):
+class AttrOp(object):
+
+    def __init__(self, text):
+        self.text = text
+
+    def get(self, data):
+        return getattr(data, self.text)
+
+    ensure = get
+
+    def set(self, data, value):
+        setattr(data, self.text, value)
+
+
+class TextOp(object):
+
+    def __init__(self, text):
+        self.text = text
+
+    def get(self, data):
+        getitem = getattr(data, '__getitem__', None)
+        if getitem is None:
+            return getattr(data, self.text)
+        else:
+            return getitem(self.text)
+
+    def ensure(self, data):
+        getitem = getattr(data, '__getitem__', None)
+        if getitem is None:
+            return getattr(data, self.text)
+        else:
+            try:
+                return getitem(self.text)
+            except KeyError:
+                data[self.text] = value = {}
+                return value
+
+    def set(self, data, value):
+        setitem = getattr(data, '__setitem__', None)
+        if setitem is None:
+            return setattr(data, self.text, value)
+        else:
+            return setitem(self.text, value)
+
+
+class ConvertOp(object):
+
+    def __init__(self, callable_):
+        self.callable = callable_
+
+    def get(self, data):
+        return self.callable(data)
+
+    def ensure(self, *args):
+        raise TypeError('Cannot use convert() as target')
+
+    set = ensure
+
+
+class InsertOp(object):
+
+    def __init__(self, index):
+        self.index = index
+
+    def get(self, data):
+        raise TypeError('Cannot use insert() in source')
+
+    def ensure(self, data):
+        value = {}
+        data.insert(self.index, value)
+        return value
+
+    def set(self, data, value):
+        data.insert(self.index, value)
+
+
+class AppendOp(object):
+
+    def get(self, data):
+        raise TypeError('Cannot use append() in source')
+
+    def ensure(self, data):
+        value = {}
+        data.append(value)
+        return value
+
+    def set(self, data, value):
+        data.append(value)
+
+
+class Path(object):
 
     def __init__(self, *ops):
         self.ops = ops
@@ -11,38 +116,43 @@ class LoadPath(object):
         return type(self)(*(self.ops + (op,)))
 
     def __getitem__(self, name):
-        return self._extend(itemgetter(name))
+        return self._extend(ItemOp(name))
 
     def __getattr__(self, name):
-        return self._extend(attrgetter(name))
+        return self._extend(AttrOp(name))
 
+    def insert(self, index):
+        return self._extend(InsertOp(index))
 
-source = LoadPath()
-
-
-def resolve_text_path(text, data):
-    for part in text.split('.'):
-        getitem = getattr(data, '__getitem__', None)
-        if getitem is None:
-            data = getattr(data, part)
-        else:
-            data = getitem(part)
-    return data
+    def append(self):
+        return self._extend(AppendOp())
 
 
 def parse_text(segment):
     if isinstance(segment, str):
-        segment = LoadPath(partial(resolve_text_path, segment))
+        segment = Path(*(TextOp(part) for part in segment.split('.')))
     return segment
 
 
 def load(data, path):
     path = parse_text(path)
     for op in path.ops:
-        data = op(data)
+        data = op.get(data)
     return data
 
 
 def convert(source, callable_):
     source = parse_text(source)
-    return source._extend(callable_)
+    return source._extend(ConvertOp(callable_))
+
+
+def store(data, path, value):
+    path = parse_text(path)
+    if not path.ops:
+        raise TypeError('Cannot store at root')
+    for op in path.ops[:-1]:
+        data = op.ensure(data)
+    path.ops[-1].set(data, value)
+
+
+source = target = Path()
--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -1,8 +1,8 @@
 from argparse import Namespace
 
-from testfixtures import compare
+from testfixtures import compare, ShouldRaise
 
-from configurator.mapping import source, load, convert
+from configurator.mapping import source, load, convert, store, target
 
 
 class TestSource(object):
@@ -46,3 +46,110 @@ class TestSource(object):
     def test_convert_string(self):
         data = Namespace(x='1')
         compare(load(data, convert('x', int)), expected=1)
+
+    def test_insert(self):
+        with ShouldRaise(TypeError('Cannot use insert() in source')):
+            load(None, source.insert(0))
+
+    def test_append(self):
+        with ShouldRaise(TypeError('Cannot use append() in source')):
+            load(None, source.append())
+
+
+class TestTarget(object):
+
+    def test_root(self):
+        data = {'foo'}
+        with ShouldRaise(TypeError('Cannot store at root')):
+            store(data, target, 'foo')
+        compare(data, expected={'foo'})
+
+    def test_getitem(self):
+        data = {'foo': 'bar'}
+        store(data, target['foo'], 'baz')
+        compare(data, expected={'foo': 'baz'})
+
+    def test_index(self):
+        data = ['a', 'b']
+        store(data, target[1], 'c')
+        compare(data, expected=['a', 'c'])
+
+    def test_append(self):
+        data = ['a', 'b']
+        store(data, target.append(), 'c')
+        compare(data, expected=['a', 'b', 'c'])
+
+    def test_append_nested(self):
+        data = []
+        store(data, target.append()['a'], 'b')
+        compare(data, expected=[{'a': 'b'}])
+
+    def test_insert(self):
+        data = ['a', 'b']
+        store(data, target.insert(1), 'c')
+        compare(data, expected=['a', 'c', 'b'])
+
+    def test_insert_nested(self):
+        data = []
+        store(data, target.insert(0)['a'], 'b')
+        compare(data, expected=[{'a': 'b'}])
+
+    def test_attr(self):
+        data = Namespace(x=1)
+        store(data, target.x, 2)
+        compare(data.x, 2)
+
+    def test_nested(self):
+        data = {'foo': ['a', 'b', Namespace(x=1)]}
+        store(data, target['foo'][2].x, 2)
+        compare(data['foo'][2].x, expected=2)
+
+    def test_string_item(self):
+        data = {'foo': 'bar'}
+        store(data, 'foo', 'baz')
+        compare(data, expected={'foo': 'baz'})
+
+    def test_string_attr(self):
+        data = Namespace(foo='bar')
+        store(data, target.x, 2)
+        compare(data.x, 2)
+
+    def test_string_dotted(self):
+        data = {'foo': Namespace(x=1)}
+        store(data, 'foo.x', 2)
+        compare(data['foo'].x, expected=2)
+
+    def test_create_nested_dicts(self):
+        # created needed dicts
+        data = {}
+        store(data, target['x']['y'], 2)
+        compare(data, expected={'x': {'y': 2}})
+
+    def test_create_nested_attrs(self):
+        # exception
+        data = {}
+        with ShouldRaise(AttributeError("'dict' object has no attribute 'x'")):
+            store(data, target.x.y, 2)
+
+    def test_create_from_dotted_string(self):
+        data = {}
+        store(data, 'x.y', 2)
+        compare(data, expected={'x': {'y': 2}})
+
+    def test_create_nested_attrs_from_dotted_string(self):
+        # exception
+        data = Namespace()
+        with ShouldRaise(AttributeError(
+            "'Namespace' object has no attribute 'x'"
+        )):
+            store(data, 'x.y', 2)
+
+    def test_set_on_convert(self):
+        data = '1'
+        with ShouldRaise(TypeError('Cannot use convert() as target')):
+            store(data, convert(target, int), 'y')
+
+    def test_ensure_on_convert(self):
+        data = '1'
+        with ShouldRaise(TypeError('Cannot use convert() as target')):
+            store(data, convert(target, int).x, 'y')
