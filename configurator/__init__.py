a95bd5c324bb72f89c21148f35fa2f2bc4cd1174
Chris Withers <chris@withers.org>
2019-03-21 10:14:53 +0000

    move these to the top level.

--- configurator/__init__.py
+++ configurator/__init__.py
@@ -1,2 +1,5 @@
 from .config import Config
 from .merge import default_mergers
+from .mapping import source, target, convert, required
+
+__all__ = 'Config', 'source', 'target', 'convert', 'required'
