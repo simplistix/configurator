3aa72eafa6ffc8bd6e883180bd746dda046155ac
Chris Withers <chris@withers.org>
2019-04-01 08:08:10 +0100

    default_mergers is part of the top-level public api

--- configurator/__init__.py
+++ configurator/__init__.py
@@ -2,4 +2,4 @@ from .config import Config
 from .merge import default_mergers
 from .mapping import source, target, convert, required
 
-__all__ = 'Config', 'source', 'target', 'convert', 'required'
+__all__ = 'Config', 'source', 'target', 'convert', 'required', 'default_mergers'
