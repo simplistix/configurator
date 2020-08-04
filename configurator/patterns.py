82a87c79901c689e29d20014d6356b88a5393cb1
Chris Withers <chris@simplistix.co.uk>
2020-08-04 17:19:26 +0100

    Add support for an optional root when using the extends pattern.

--- configurator/patterns.py
+++ configurator/patterns.py
@@ -2,13 +2,15 @@
 Helpers for specific patterns of usage.
 """
 
-from configurator import Config
+from . import Config
 
 
-def load_with_extends(path, key):
+def load_with_extends(path, key, root=None):
     configs = []
     while path:
         config = Config.from_path(path)
+        if root is not None and root in config:
+            config = config[root]
         configs.append(config)
         path = config.get(key)
     config = Config()
--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -1,9 +1,24 @@
+import json
+
 from testfixtures import compare
 
 from configurator import Config
 from configurator.patterns import load_with_extends
 
 
-def test_load_with_extends_key_not_present(dir):
-    path = dir.write('file.json', '{"key":"value"}')
-    compare(load_with_extends(path, key='extends'), expected=Config({'key': 'value'}))
+class TestLoadWithExtends(object):
+
+    def test_key_not_present(self, dir):
+        path = dir.write('file.json', '{"key":"value"}')
+        compare(load_with_extends(path, key='extends'), expected=Config({'key': 'value'}))
+
+    def test_root(self, dir):
+        path1 = dir.write('file1.json', '{"root": {"f1key":"f1value"}}')
+        path2 = dir.write('file2.json', json.dumps({'root': {
+            'f2key': 'f2value',
+            'extends': path1,
+        }}))
+        compare(load_with_extends(path2, key='extends', root='root'), expected=Config({
+            'f1key': 'k1value',
+            'f2key': 'f2value',
+        }))
