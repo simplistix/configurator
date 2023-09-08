c66336906123c5a56e0e96f9a0037fd40ffb933f
wim glenn <hey@wimglenn.com>
2023-09-07 21:06:57 -0500

    address the coverage miss

--- tests/test_toml_loading.py
+++ tests/test_toml_loading.py
@@ -4,10 +4,18 @@ from testfixtures import compare
 from configurator import Config
 
 
-def test_load_toml_from_path(tmp_path):
+def test_load_toml_from_path_implicit_parser(tmp_path):
     path = tmp_path / 'test.toml'
     path.write_bytes(b'k = "v"')
     config = Config.from_path(path)
+    compare(config.k, "v")
+
+
+def test_load_toml_from_path_explicit_parser(tmp_path):
+    path = tmp_path / 'test.toml'
+    path.write_bytes(b'k = "v"')
+    parser = Config.parsers['toml']
+    config = Config.from_path(path, parser)
     compare(config.k, "v")
 
 
