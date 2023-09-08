2192c75942ed31936ea77a090a1833b397b9a248
wim glenn <hey@wimglenn.com>
2023-09-07 21:01:53 -0500

    drop 3.6?

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     zip_safe=False,
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     extras_require=dict(
         yaml=['pyyaml'],
         toml=['tomli; python_version < "3.11"'],
