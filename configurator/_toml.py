195f0bfc23b88f379b17bccc51128af982d9b2f5
wim glenn <hey@wimglenn.com>
2023-09-11 20:01:46 -0500

    positional only args require 3.8

--- configurator/_toml.py
+++ configurator/_toml.py
@@ -6,7 +6,7 @@ except ImportError:
     import tomli as tomllib
 
 
-def load(f, /, *, parse_float=float):
+def load(f, *, parse_float=float):
     # wrapper around tomllib.load to be more forgiving of streams opened in text mode
     if isinstance(f, io.TextIOWrapper):
         return tomllib.load(f.buffer, parse_float=parse_float)
